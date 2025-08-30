59cfc4d64b3e449bc6aabae3b045cbcd2394832e
scherepi <48649270+scherepi@users.noreply.github.com>
2025-08-30 10:31:50 -0500

    pushing for desktop work

--- player.gd
+++ player.gd
@@ -2,8 +2,12 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
+func _process(delta: float) -> void:
+	# Handle restarts?
+	if Input.is_action_just_pressed("reset"):
+		position = Vector2(0.0, 0.0)
+		velocity = Vector2(0.0, 0.0)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,16 +15,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("accept"):
 		if velocity.y > 0:
 			velocity.y = velocity.y * -1.3
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
 
 	move_and_slide()
